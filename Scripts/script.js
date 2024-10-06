b351e9c98d280261de1425be678e6fdf934f0ad7
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-10-06 11:21:38 +0600

    added catergories and active button

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,3 +12,65 @@ document.addEventListener('click', (event) => {
         mobileMenu.classList.add('hidden')
     }
 })
+
+/*------- APIs -------*/
+const categoriesURL = `https://openapi.programming-hero.com/api/peddy/categories` // all categories
+const categoryURL = `https://openapi.programming-hero.com/api/peddy/category/` // single category by name
+const petsURL = `https://openapi.programming-hero.com/api/peddy/pets` // all pets
+const petURL = `https://openapi.programming-hero.com/api/peddy/pet/` // single pet by id
+
+/*------- fetch categories -------*/
+let categoryList = []
+
+const fetchCategories = async () => {
+
+    try {
+      const response = await fetch(categoriesURL)
+      const data = await response.json();
+      categoryList = await data.categories
+    } catch (error) {
+      console.error('There was some issue while loading categories:', error)
+    }
+}
+  
+async function displayCategories(){
+    await fetchCategories()
+    const categoryParent = document.getElementById('catagories')
+    categoryParent.innerHTML = ''
+
+    categoryList.forEach(cat => {
+        const newCat = document.createElement('div')
+        newCat.innerHTML = 
+        `
+            <div class='md:w-[3rem] w-[1.3rem]'>
+                <img src = '${cat.category_icon}' alt = '${cat.category}' class='w-full'>
+            </div>
+            <span class='font-bold md:text-xl text-base'>${cat.category}s</span>
+        `
+        newCat.classList.add('md:px-12', 'md:py-4', 'p-3', 'flex', 'md:gap-4', 'gap-2', 'justify-center', 'items-center',  'cursor-pointer', 'transition', 'duration-300', 'inactive')
+        newCat.setAttribute('id', `cat-${cat.id}`)
+    
+        categoryParent.appendChild(newCat)
+
+        newCat.addEventListener('click', (event) => {
+            activeCat(event, newCat)
+        })
+    })
+}
+
+displayCategories()
+
+/*------- active category -------*/
+const activeCat = (event, newCat) => {
+    // console.log(newCat.id)
+
+    for(let i = 0; i < categoryList.length; i++){
+        document.getElementById(`cat-${i+1}`).classList.remove('active')
+        document.getElementById(`cat-${i+1}`).classList.add('inactive')
+        console.log(document.getElementById(`cat-${i+1}`))
+    }
+    
+    newCat.classList.remove('inactive')
+    newCat.classList.add('active')
+}
+  
